8132b27ed64acfef8707cd1a9870d4c03ea0342d
Georgios Karametas <geometas@hotmail.com>
2017-11-09 14:06:34 +0100

    Added price textviews and handler

--- app/src/main/java/work/geodoo/justjava/MainActivity.java
+++ app/src/main/java/work/geodoo/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.icu.text.NumberFormat;
 
 /**
 
@@ -17,19 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-
-     This method is called when the order button is clicked.
-     */
     public void submitOrder(View view) {
-        display(1);
+        display(2);
+        displayPrice(2*5);
     }
-    /**
 
-     This method displays the given quantity value on the screen.
-     */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }

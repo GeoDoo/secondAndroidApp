55173cb1144512f1c76597d2bb9a76a63fd326b6
Georgios Karametas <geometas@hotmail.com>
2017-11-10 15:53:37 +0100

    Added checkbox for whipped cream

--- app/src/main/java/work/geodoo/justjava/MainActivity.java
+++ app/src/main/java/work/geodoo/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package work.geodoo.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.icu.text.NumberFormat;
 
@@ -12,7 +13,8 @@ import android.icu.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 2;
+    int quantity = 0;
+    boolean whippedCream = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +27,20 @@ public class MainActivity extends AppCompatActivity {
         displayMessage(message);
     }
 
+    public void checkWhippedCream(View view) {
+        whippedCream = !whippedCream;
+    }
+
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
     private String setMessage() {
-        return "Total: " + NumberFormat.getCurrencyInstance().format(quantity * 5) + "\nThank you!";
+        if (whippedCream) {
+            return "Total: " + NumberFormat.getCurrencyInstance().format(quantity * 5) + "\nAdd whipped cream too? Yes" + "\nThank you!";
+        }
+        return "Total: " + NumberFormat.getCurrencyInstance().format(quantity * 5) + "\nAdd whipped cream too? No" + "\nThank you!";
     }
 
     private void displayMessage(String message) {
